6e8d538aff5323c3cfd27ffc576a13fa82dcea6f
Krishna Ghodke <krushnaghodke90@gmail.com>
2025-05-22 12:33:29 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Hello from Jenkins with PHP</title>
+</head>
+<body>
+  <h1>Hello from Jenkins</h1>
+  <h1>✅ Push Testing</h1>
+  <h1>✅ Slack Testing</h1>
+  <h2>PHP Version: <?php echo phpversion(); ?></h2>
+</body>
+</html>
