d20b09e5434fbe17cd4af1dd4b4001104c3fa7c6
Krishna Ghodke <krushnaghodke90@gmail.com>
2025-05-22 11:28:04 +0000

    login application

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($user) {
+            print(password_verify($password, $user['password']));
             if (password_verify($password, $user['password'])) {
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['user_name'] = $user['username'];
